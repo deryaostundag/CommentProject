60f6eb0456cdc80735e30cdf473579e122eb8aa4
deryaostundag <dryostundag@gmail.com>
2023-04-18 16:47:56 +0300

    Add project files.

---
+++ CommentProject/Controllers/TitleController.cs
@@ -0,0 +1,41 @@
+﻿using CommentProject.BusinessLayer.Abstract;
+using CommentProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CommentProject.Controllers
+{
+    public class TitleController : Controller
+    {
+        private readonly ITitleService _titleService;
+        private readonly ICategoryService _categoryService;
+        public TitleController(ITitleService titleService, ICategoryService categoryService) 
+        {
+            _categoryService = categoryService;
+            _titleService = titleService;
+        }
+
+        public IActionResult Index()
+        {
+            var values=_titleService.TGetList();
+            return View(values);
+        }
+        [HttpGet] 
+        public IActionResult AddTitle()
+        {
+            List<SelectListItem> values = (from x in _categoryService.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;                             
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTitle(Title title)
+        {
+            return View();
+        }
+    }
+}
---
+++ CommentProject/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CommentProject.Models
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
